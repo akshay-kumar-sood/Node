386bc94be2c6ddcb4930e64af42e7adb5ddb4a7f
akshay-kumar-sood <id.akshay23@gmail.com>
2025-07-02 00:30:18 +0530

    remove unwanted files

--- sample.js
+++
@@ -1,9 +0,0 @@
-const fs=require('fs');
-fs.writeFile('./hello.txt',"creating a sample hello file",(err)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log("data added successfully");
-    }
-});
